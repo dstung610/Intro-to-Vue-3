980697cba76c23b780ac12fdf071006cf4c2bb9d
Son Tung Do <dstung610@gmail.com>
2021-08-03 00:46:51 +0200

    L5 completed

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 1, color: 'green'},
+                {id: 2, color: 'blue'}
+            ],
+            sizes: [
+                {id: 4, size: 12},
+                {id: 5, size: 13}
+            ]
         }
     }
 })
