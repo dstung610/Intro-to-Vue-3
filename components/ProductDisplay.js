5513d3a44a9688f16961cc4ae8a9240bef0c1ac7
Son Tung Do <dstung610@gmail.com>
2021-08-03 16:01:50 +0200

    L10 completed

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,9 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button class='button' @click="removeFromCart">
+            Remove item
+        </button>
       </div>
     </div>
   </div>`,
@@ -56,10 +59,13 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+      },
+      removeFromCart() {
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
-          this.selectedVariant = index
+            this.selectedVariant = index
       }
   },
   computed: {
